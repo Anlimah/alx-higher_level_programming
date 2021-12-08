95a6407086f65017051039cf0d455828b1112c45
Francis Anlimah <17730459+Anlimah@users.noreply.github.com>
2021-12-08 12:54:11 +0000

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,15 +2,18 @@
 """ Creates an empty class called Square
 """
 
+
 class Square:
-    """ A class that defines a square by its size
+    """ Empty class with size private attribute
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
         """
-        if not isinstance(size, int):
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
