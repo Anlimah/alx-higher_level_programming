a7a232bc4e28f7a238927741803735dcb219ae47
Francis Anlimah <17730459+Anlimah@users.noreply.github.com>
2021-12-08 13:25:41 +0000

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,25 +1,48 @@
 #!/usr/bin/python3
+""" Creates an empty class called Square
+"""
+
+
 class Node:
+    
     def __init__(self, data, next_node=None):
+        """
+                Instantiation with size
+        Args:
+            data: data of the node
+            next_node: next node
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        Returns data
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        sets data of node
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """
+        Returns next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        sets next node value
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
